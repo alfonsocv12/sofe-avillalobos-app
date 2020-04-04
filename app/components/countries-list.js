29f1101bf8436d81415430a531b4f9771ced1aec
alfonsocv12 <alfonsocvu@gmail.com>
2020-04-04 06:18:53 -0700

    Ading first component

--- app/components/countries-list.js
+++ app/components/countries-list.js
@@ -7,15 +7,47 @@ import { A } from '@ember/array';
 export default class CountriesListComponent extends Component {
   @tracked countries = A(this.args.countries);
 
+  @tracked item;
+
+  sortByItems = [
+    {
+      "name": "Total Cases",
+      "query": "cases"
+    },
+    {
+      "name": "Today Cases",
+      "query": "todayCases"
+    },
+    {
+      "name": "Total Deaths",
+      "query": "deaths"
+    },
+    {
+      "name": "today Deaths",
+      "query": "todayDeaths"
+    },
+    {
+      "name": "Total Recoveries",
+      "query": "recovered"
+    }
+  ]
+
   @action
-  async change(query){
-    query = query.target.value;
+  change(query){
+    query = query.target.value.toLowerCase();
     this.countries = A(this.args.countries.filter(
       obj => {
-        if(obj.country.includes(query)){
+        if(obj.country.toLowerCase().includes(query)){
           return obj
         }
       }
     ));
+  }
+
+  @action
+  async changeItems(item, closeDropdown){
+    this.countries = await jQuery.get(
+      "http://api.coronastatistics.live/countries/?sort="+item.query
+    );
   }
 }
