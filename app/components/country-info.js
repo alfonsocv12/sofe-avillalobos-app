3647e5aa11cd3201df27e793172ec9a5441ac72a
alfonsocv12 <alfonsocvu@gmail.com>
2020-04-04 23:46:21 -0700

    Se agrego lo que funciona

---
+++ app/components/country-info.js
@@ -0,0 +1,10 @@
+import Component from '@glimmer/component';
+
+export default class CountryInfoComponent extends Component {
+  countries = [
+    {"pais":"mexico"},
+    {"pais":"china"},
+    {"pais":"usa"}
+  ]
+
+}
