f4e0332d549c8068578d387cfefc824697abe3fb
alfonsocv12 <alfonsocvu@gmail.com>
2020-04-04 18:38:00 -0700

    Adding new style to de mix

---
+++ app/components/map.js
@@ -0,0 +1,7 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { A } from '@ember/array';
+
+export default class MapComponent extends Component {
+
+}
