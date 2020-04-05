a88e933d09f7030df0b4433818d3e9b2170b888a
alfonsocv12 <alfonsocvu@gmail.com>
2020-04-04 18:59:55 -0700

    Create first chart

--- app/components/map.js
+++ app/components/map.js
@@ -1,7 +1,102 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { A } from '@ember/array';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
 
 export default class MapComponent extends Component {
+  @tracked map = this.loadMap();
 
+  init() {
+    this._super(...arguments);
+    this.chart = null;
+  }
+
+  loadMap(){
+    let chart = am4core.create("mapdiv", am4charts.PieChart);
+
+    // Create pie series
+    let series = chart.series.push(new am4charts.PieSeries());
+    series.dataFields.value = "litres";
+    series.dataFields.category = "country";
+
+    // Add data
+    chart.data = [{
+      "country": "Lithuania",
+      "litres": 501.9
+    }, {
+      "country": "Czech Republic",
+      "litres": 301.9
+    }, {
+      "country": "Ireland",
+      "litres": 201.1
+    }, {
+      "country": "Germany",
+      "litres": 165.8
+    }, {
+      "country": "Australia",
+      "litres": 139.9
+    }, {
+      "country": "Austria",
+      "litres": 128.3
+    }, {
+      "country": "UK",
+      "litres": 99
+    }, {
+      "country": "Belgium",
+      "litres": 60
+    }, {
+      "country": "The Netherlands",
+      "litres": 50
+    }];
+
+    // And, for a good measure, let's add a legend
+    chart.legend = new am4charts.Legend();
+  }
+
+  didInsertElementos() {
+    this._super(...arguments);
+
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+    chart.paddingRight = 20;
+
+    let data = [];
+    let visits = 10;
+    for (let i = 1; i < 366; i++) {
+      visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+      data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
+    }
+
+    chart.data = data;
+
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    let series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+
+    series.tooltipText = "{valueY.value}";
+    chart.cursor = new am4charts.XYCursor();
+
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(series);
+    chart.scrollbarX = scrollbarX;
+
+    this.chart = chart;
+  }
+
+  willDestroyElementos() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+    this._super(...arguments);
+  }
 }
